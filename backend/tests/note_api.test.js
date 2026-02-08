7e22f209771c5ab76fa061ad9c8c37e8eb8f466e
David Buller <bullerd@comcast.net>
2026-02-08 15:57:57 -0500

    modify Mongodb tests

--- backend/tests/note_api.test.js
+++ backend/tests/note_api.test.js
@@ -1,11 +1,38 @@
-const { test, after } = require("node:test");
+const { test, after, beforeEach, before } = require("node:test");
 const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const logger = require("../utils/logger");
 const app = require("../app");
+const Note = require("../models/note");
 
 const api = supertest(app);
+
+const initialNotes = [
+  {
+    content: "Buller's MongoDB lesson 12/14/25",
+    important: false,
+  },
+  {
+    content: "A second MongoDB record has been added",
+    important: true,
+  },
+];
+
+//From FSO
+// beforeEach(async () => {
+//   await Note.deleteMany({});
+//   initialNotes.forEach(async (n) => {
+//     const newNote = Note(n);
+//     await newNote.save();
+//   });
+// });
+
+//From Claude and me:
+beforeEach(async () => {
+  await Note.deleteMany({});
+  await Note.insertMany(initialNotes);
+});
 
 test("notes are returned as json", async () => {
   await api
@@ -16,9 +43,9 @@ test("notes are returned as json", async () => {
 
 test("all notes are returned", async () => {
   response = await api.get("/api/notes");
-  logger.info(response.body);
+  // logger.info(response.body);
 
-  assert.strictEqual(response.body.length, 2);
+  assert.strictEqual(response.body.length, initialNotes.length);
 });
 
 test("a specific note is returned", async () => {
@@ -29,6 +56,8 @@ test("a specific note is returned", async () => {
     contents.includes("Buller's MongoDB lesson 12/14/25"),
     true,
   );
+  // could have simplified this the above to:
+  // assert(contents.includes("Buller's MongoDB lesson 12/14/25"));
 });
 after(async () => {
   await mongoose.connection.close();
