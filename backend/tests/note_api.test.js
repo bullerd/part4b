322d1109b874538d4d44f3bd3350ab42c47b9340
David Buller <bullerd@comcast.net>
2026-02-10 16:42:56 -0500

    add api tests

--- backend/tests/note_api.test.js
+++ backend/tests/note_api.test.js
@@ -59,6 +59,39 @@ test("a specific note is returned", async () => {
   // could have simplified this the above to:
   // assert(contents.includes("Buller's MongoDB lesson 12/14/25"));
 });
+
+test("a valid note can be added ", async () => {
+  const newNote = {
+    content: "async/await makes this much easier",
+    important: true,
+  };
+  await api
+    .post("/api/notes")
+    .send(newNote)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await api.get("/api/notes");
+
+  const contents = response.body.map((r) => r.content);
+  assert.strictEqual(response.body.length, initialNotes.length + 1);
+
+  assert(contents.includes("async/await makes this much easier"));
+});
+
+test("note without content is not added", async () => {
+  const newNote = {
+    important: true,
+  };
+
+  await api.post("/api/notes").send(newNote).expect(400);
+
+  const response = await api.get("/api/notes");
+  // console.log(response.body.map((n) => n.content));
+
+  assert.strictEqual(response.body.length, initialNotes.length);
+});
+
 after(async () => {
   await mongoose.connection.close();
 });
